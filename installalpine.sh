281abecc7145e4e6a940ce32aa12cadb37ce035d
marmocchio8 <40793698+marmocchio8@users.noreply.github.com>
2020-08-08 20:27:29 +0200

    Fff

--- installalpine.sh
+++ installalpine.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 adduser -D tmp
-apk add build-base curl
-su - tmp
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+apk add build-base curl sudo
+sudo -u tmp curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 su
 apk add git
-su - tmp
-cd /
-git clone https://github.com/marmocchio8/Fileserver.git
-cp -r Fileserver/ .
-rm installalpine.sh
-cargo build
+sudo -u tmp "cd / && git clone https://github.com/marmocchio8/Fileserver.git && cp -r Fileserver/ . && rm installalpine.sh && cargo build" 
 exit
 
