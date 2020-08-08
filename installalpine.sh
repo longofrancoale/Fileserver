9c30fc08b298f1c68e74a6c323573b8bb5cca505
marmocchio8 <40793698+marmocchio8@users.noreply.github.com>
2020-08-08 18:45:34 +0200

    Correzione errori

--- installalpine.sh
+++ installalpine.sh
@@ -2,11 +2,11 @@
 
 adduser -D tmp
 apk add build-base curl
-su tmp
+su - tmp
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 su
 apk add git
-su tmp
+su - tmp
 cd /
 git clone https://github.com/marmocchio8/Fileserver.git
 cp -r fileserver/ .
