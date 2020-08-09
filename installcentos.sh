2503a3b98f52056d8e03773d1e5ac8fa5fe27ee4
user.name <longofrancoale@gmail.com>
2020-08-09 09:24:30 +0000

    installcentos.sh

---
+++ installcentos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo yum install git curl 
+sudo yum group install "Development Tools"
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env 
+cd /
+git clone https://github.com/marmocchio8/Fileserver
+cp -r Fileserver/ .
+rm installcentos.sh
+cargo build
