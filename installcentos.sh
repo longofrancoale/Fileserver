f7c2d81af804e68dd7a9ffce46f06afde2b7764e
marmocchio8 <40793698+marmocchio8@users.noreply.github.com>
2020-08-09 17:35:40 +0200

    Ttttttttttt

--- installcentos.sh
+++ installcentos.sh
@@ -7,10 +7,10 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env 
 cd $HOME
 git clone https://github.com/marmocchio8/Fileserver
-chmod -r 777 Fileserver
+chmod 777 -r Fileserver
 su -c "cp -r Fileserver /"
 cd /
-su -c "chmod -r 7777 Fileserver"
+su -c "chmod 777 -r Fileserver"
 su -c "cp -r Fileserver/* ."
 rm installcentos.sh
 cargo build
