f9694ca5a9bfcd6bdf03efdab9043a298c212676
marmocchio8 <40793698+marmocchio8@users.noreply.github.com>
2020-08-09 17:25:56 +0200

    Altri ettori yeee

--- installcentos.sh
+++ installcentos.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 su -c "yum install git curl"
-su -c "yum group install "Development Tools""
+su -c "yum group install 'Development Tools'"
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env 
+cd $HOME
+git clone https://github.com/marmocchio8/Fileserver
+chmod -r 7777 Fileserver
+su -c "cp -r Fileserver /"
 cd /
-su -c "git clone https://github.com/marmocchio8/Fileserver"
-su -c "chmod 7777 Fileserver"
-cp -r Fileserver/ .
+su -c "chmod -r 7777 Fileserver"
+su -c "cp -r Fileserver/* ."
 rm installcentos.sh
 cargo build
